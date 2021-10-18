8962fbec973e29ae42ce527aa315fa37076d770a
Christy C <91734372+Christykarunya@users.noreply.github.com>
2021-10-18 16:57:50 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,16 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+		System.out.println("Name of the College: Anna University");
+
+	}
+	public void collegeCode() {
+		System.out.println("College code: 10021");
+	}
+	public void collegeRank() {
+		System.out.println("College Rank: 1");
+
+	}
+
+}
